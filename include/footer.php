1b2e2243abeb4f97f986171124cfa60f93623be1
OUAIS MON FRERE <jeralexis77@aol.com>
2021-11-24 17:03:30 +0100

    footer

--- include/footer.php
+++ include/footer.php
@@ -4,12 +4,12 @@
     <div class="footer-jaune">
         <p class="footer-jaune-titre">T’as tout pour toi !</p>
         <p class="footer-jaune-soustitre">Un jour on parlera peut-être de toi dans cette newsletter, il ne faudrait pas manquer ça !
-Alors, abonne-toi !</p>
+            <br>Alors, abonne-toi !</p>
 
-        <form>
+        <form class="footer-form">
             <div>
-                <input id="example" type="text" name="text" placeholder="reussite@gmail.com">
-                <input type="submit" value="Je m’incris">
+                <input id="footer-newsletter" type="text" name="text" placeholder="reussite@gmail.com">
+                <input id="footer-submit" type="submit" value="Je m’incris">
             </div>
         </form>
         <div class="footer-jaune-resaux">
