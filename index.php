4cd580cbe272abadcab2c266ab6d9a94f145dfef
justineyg <justineyang99@gmail.com>
2021-11-24 17:00:41 +0100

    parties juju

--- index.php
+++ index.php
@@ -23,28 +23,40 @@
 
     <section class="advices">
         <h2>Nos conseils pour te lancer</h2>
-        <p>Tu l’ignores peut-être mais tu es dans la meilleure période de ta vie pour entreprendre ! Il te suffit de beaucoup de passion, d’un peu d’organisation, d’une dose de discipline et d’une pincée de motivation avant de devenir ton propre patron ! </p>
+        <p class="paragraph">Tu l’ignores peut-être mais tu es dans la meilleure période de ta vie pour entreprendre ! Il te suffit de beaucoup de passion, d’un peu d’organisation, d’une dose de discipline et d’une pincée de motivation avant de devenir ton propre patron ! </p>
 
-        <div>
+        <div class="three">
             <div class="time">
             <span class="iconify" data-icon="ic:baseline-favorite-border" style="color: #5c7aea; font-size: 55px;"></span>
-            <p>Optimise ton temps</p>
+            <p class="little_title">Optimise ton temps</p>
             <p>On sait très bien que tu passes beaucoup trop de temps sur Tiktok... Utilise le autrement !</p>
             </div>
 
             <div class="inform">
             <span class="iconify" data-icon="ic:round-hourglass-bottom" style="color: #5c7aea; font-size: 55px;"></span>
-            <p>Renseignes toi !</p>
+            <p class="little_title">Renseignes toi !</p>
             <p>Il y a beaucoup d’avantages à ta disposition, nous sommes là pour t’aider à les trouver !</p>
             </div>
 
             <div class="trust">
             <span class="iconify" data-icon="ic:baseline-connected-tv" style="color: #5c7aea; font-size: 55px;"></span>
-            <p>Aie confiance en toi !</p>
+            <p class="little_title">Aie confiance en toi !</p>
             <p>Malgré certaines contraintes et quelques freins, tu as tout pour te lancer !</p>
             </div>
-
         </div>
+        </section>
+
+        <section class="part_two">
+            <div class="left_side">
+                <h2>Les clés de la confiance en soi</h2>
+                <p>Tout est dans la tête, une simple idée peut devenir une idée de génie. Alors qu’est-ce que tu attends ?</p>
+                <a class="btn_articles" href="">Découvrir l'article</a>
+            </div>
+
+            <div class="right_side">
+                <img src="public/img/undraw_meditation.svg">
+            </div>
+        
     </section>
 
     </main>
