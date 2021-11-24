3f20a66d23ba64ac3a64b9730641e3b18f8f85b4
Yarline <yarlinecineus@outlook.fr>
2021-11-24 15:51:31 +0100

    banner video
    debut banner

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <html lang="fr">
 
 <head>
-<?php include 'include/head.php'; ?>
+    <?php include 'include/head.php'; ?>
     <title>Accueil</title>
 </head>
+
 <body>
     <header>
-    <?php include 'include/header.php'; ?>
+        <?php include 'include/header.php'; ?>
     </header>
     <main>
         <section class="banner">
-
+            <video class="bannerVideo">
+                <source src=public/video/prod.mp4 type=video/mp4>
+            </video>
         </section>
 
     </main>
     <footer>
-    <?php include 'include/footer.php'; ?>
+        <?php include 'include/footer.php'; ?>
     </footer>
 </body>
 
