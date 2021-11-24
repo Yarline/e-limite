5da7c203a0a726eb438d64a5b76f9817b55fa95c
Yarline <yarlinecineus@outlook.fr>
2021-11-24 15:31:19 +0100

    fichier include
    head header footer

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <html lang="fr">
+
 <head>
+<?php include 'include/head.php'; ?>
     <title>Accueil</title>
 </head>
 <body>
-    
+    <header>
+    <?php include 'include/header.php'; ?>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+    <?php include 'include/footer.php'; ?>
+    </footer>
 </body>
+
 </html>
