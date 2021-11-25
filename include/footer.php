a21df12f1d12d5a05c7f46b22ec19b1463c572c6
justineyg <justineyang99@gmail.com>
2021-11-25 13:59:21 +0100

    cookies

--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,9 @@
 <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
-<script type="text/javascript" src="https://cookieconsent.popupsmart.com/src/js/popper.js"></script><script> window.start.init({Palette:"palette2",Mode:"banner bottom",Theme:"classic",Message:"Ce site utilise des cookies pour personnaliser et améliorer votre confort de navigation. ",ButtonText:"Tout accepter",LinkText:"En savoir plus",Time:"1",})</script>
+
+<script type="text/javascript" src="https://cookieconsent.popupsmart.com/src/js/popper.js"></script><script> window.start.init({Palette:"palette2",Mode:"banner bottom",Theme:"block",Message:"Ce site utilise des cookies pour personnaliser et améliorer votre confort de navigation. ",ButtonText:"Tout accepter",LinkText:"En savoir plus",Time:"1",})</script>
+
 <script src="public/js/script.js"></script>
 <div>
     <div class="footer-jaune">
