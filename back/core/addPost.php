49782989488215faa09a50cddbe609ce274f25a0
Mistrale94 <quat94@gmail>
2021-11-26 16:46:20 +0100

    texte flash_in

--- back/core/addPost.php
+++ back/core/addPost.php
@@ -16,7 +16,7 @@ if(empty($_POST['title'])){
 	exit();
 }
 if(empty($_POST['body'])){	
-	flash_in('error', 'La date ne peut pas être vide.');
+	flash_in('error', 'Le contenu ne peut pas être vide.');
 	header('Location: ../addPost.php');
 	exit();
 }
--- back/core/addTestimony.php
+++ back/core/addTestimony.php
@@ -11,17 +11,17 @@ if (!isset($_SESSION['user'])) {
 $_POST = array_map('trim', $_POST);
 
 if(empty($_POST['body'])){	
-	flash_in('error', 'Le titre ne peut pas être vide.');
+	flash_in('error', 'Le contenu ne peut pas être vide.');
 	header('Location: ../addPost.php');
 	exit();
 }
 if(empty($_POST['name'])){	
-	flash_in('error', 'La date ne peut pas être vide.');
+	flash_in('error', 'Le nom ne peut pas être vide.');
 	header('Location: ../addPost.php');
 	exit();
 }
 if(empty($_POST['entreprise'])){	
-	flash_in('error', 'La date ne peut pas être vide.');
+	flash_in('error', 'Le nom d\'entreprise ne peut pas être vide.');
 	header('Location: ../addPost.php');
 	exit();
 }
