a66d51d98464884546c727b0e7a93d3c3c6d4b2a
justineyg <justineyang99@gmail.com>
2021-11-26 09:36:26 +0100

    Ancres

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <nav>
-    <a href="">Conseils</a>
-    <a href="">Success story</a>
-    <a href="">Actualités</a>
+    <a href="#avdices-ancre">Conseils</a>
+    <a href="#sucessStory-ancre">Success story</a>
+    <a href="#actus-ancre">Actualités</a>
     <a href="">Parrainage</a>
     <a href="" class="header-button filled-btn">Lance-toi</a>
 </nav>
