457061431a28c71be925978d76db1722ede97b12
justineyg <justineyang99@gmail.com>
2021-12-02 16:42:53 +0100

    change

--- include/header.php
+++ include/header.php
@@ -5,5 +5,5 @@
     <a href="">Parrainage</a>
     <a href="" class="header-button filled-btn">Lance-toi</a>
 
-    <a id="burger"></a>
+    <a id="burger"><span class="iconify" data-icon="icon-park:hamburger-button"></span></a>
 </nav>
