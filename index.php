9329eba4a819487f2f8c9c738447d892f79ebef3
Yarline <yarlinecineus@outlook.fr>
2021-12-02 15:54:39 +0100

    delete slick slider

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $tPost = $post->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <?php include 'include/head.php'; ?>
     <link rel="stylesheet" href="public/css/style.css">
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <title>Accueil</title>
 
 </head>
