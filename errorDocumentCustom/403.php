442b3aa5df15f4024ebdd1291b268c979e71a83e
Mistrale94 <quat94@gmail>
2021-12-02 16:04:14 +0100

    erreur 403 404

---
+++ errorDocumentCustom/403.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../config/settings.php');
+$title = "Erreur 404";
+require_once('../includes/header.php');
+
+?><!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <?php include('../includes/head.php'); ?>
+        <title>Error 403</title>
+    </head>
+    <body>
+
+        <h1>Error 403</h1>
+
+    </body>
+</html>
