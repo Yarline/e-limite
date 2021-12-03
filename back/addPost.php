6d2a16b7573499dc267ab254932113c4a2d95ab5
OUAIS MON FRERE <jeralexis77@aol.com>
2021-12-03 11:51:57 +0100

    1er dose

--- back/addPost.php
+++ back/addPost.php
@@ -11,29 +11,33 @@ if (!isset($_SESSION['user'])) {
 ?><!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="../public/css/reset.css">
+	<link rel="stylesheet" href="../public/css/style.css">
+	<link rel="stylesheet" href="css/backoffice.css">
 	<?php include('../include/head.php'); ?>
-	<link rel="stylesheet" href="">
-	<title>Ajouter un post</title>
 </head>
 <body>
-	<?php include('../include/header.php'); ?>
 	<main>
-		<section class="item">
-			<h1>Ajouter un post</h1>
-			<form method="POST" action="core/addPost.php" enctype="multipart/form-data">
-				<p class="addFile">
-                    <label for="more">Image</label>
-                    <input type="file" name="fichier" id="more" required>
-                    <div id="display-image"></div>
-                </p>
-				<p>
-					<textarea type="text" name="body" placeholder="Votre contenu" required></textarea>
-				</p>
-				<p>
-					<button type="submit">Valider</button>
-				</p>
-			</form>
-		</section>
+		<div class="login-form-container">
+			<?php include('../include/navAdmin.php'); ?>
+			<section class="item new-posts-form">
+				<h1>Nouveau post</h1>
+				<form method="POST" action="core/addPost.php" enctype="multipart/form-data">
+					<p class="addFile">
+						<label for="more">Image</label>
+						<input type="file" name="fichier" id="more" required>
+						<div id="display-image"></div>
+					</p>
+					<p class="addContent">
+						<label>Contenu</label>
+						<textarea type="text" name="body" placeholder="Votre contenu" required></textarea>
+					</p>
+					<p>
+						<button type="submit">Valider</button>
+					</p>
+				</form>
+			</section>
+		</div>
 	</main>
 </body>
 </html>
---
+++ include/navAdmin.php
@@ -0,0 +1,22 @@
+<aside class="new-posts">
+    <figure class="nav_logo">
+        <img src="../public/img/logo.png" alt="logo">
+    </figure>
+        <div class="new-posts-group">
+            <div class="new-posts-group-links">
+                <a href="../back/addTestimony.php">
+                    <img src="../public/img/plus.svg" alt="">Témoignages
+                </a>
+            </div>
+            <div class="new-posts-group-links link-active">
+                <a href="../back/addPost.php">
+                    <img src="../public/img/plus.svg" alt="">Posts
+                </a>
+            </div>
+        </div>
+    <div class="new-posts-group-links link-logout">
+        <a href="../back/core/logout.php">
+            <img src="../public/img/logout.svg" alt="">Se déconnecter
+        </a>
+    </div>
+</aside>
