f936c30b640664f78ddbeec81498cc152626b181
Mistrale94 <quat94@gmail>
2021-12-03 15:39:19 +0100

    404 403 style

--- errorDocumentCustom/404.php
+++ errorDocumentCustom/404.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
+        <?php include 'include/head.php'; ?>
+        <link rel="stylesheet" href="public/css/style.css">
+        <link rel="stylesheet" href="public/css/reset.css">
         <title>Error 404</title>
     </head>
     <body>
-
-        <h1>Error 404</h1>
-
+        <section class="nav-error">
+            <a href="#" onclick="history.go(-1); return false;">Page précédente</a>
+            <a href="/">Acceuil</a>
+        </section>
+        <section class="body-error">
+            <div class="img-text-error">
+                <img src="public/img/404.png" alt="">
+                <div>
+                    <h1>Error 404</h1>
+                    <h2>Cette page ne semble pas exister.</h2>
+                </div>
+            </div>
+        </section>
     </body>
 </html>
