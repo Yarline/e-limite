6e51f42ea1ec795d096691956707a73a983b5f7b
Yarline <yarlinecineus@outlook.fr>
2021-12-06 17:02:48 +0100

    mailchimp
    newsletter souscription

--- include/footer.php
+++ include/footer.php
@@ -1,16 +1,18 @@
 <script src="https://code.iconify.design/2/2.1.0/iconify.min.js"></script>
 
 <script>
-window.axeptioSettings = {
-  clientId: "61a883d53ce7ad35c37e37f5",
-  cookiesVersion: "t'as tout pour toi-base",
-};
- 
-(function(d, s) {
-  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-  e.async = true; e.src = "//static.axept.io/sdk.js";
-  t.parentNode.insertBefore(e, t);
-})(document, "script");
+    window.axeptioSettings = {
+        clientId: "61a883d53ce7ad35c37e37f5",
+        cookiesVersion: "t'as tout pour toi-base",
+    };
+
+    (function(d, s) {
+        var t = d.getElementsByTagName(s)[0],
+            e = d.createElement(s);
+        e.async = true;
+        e.src = "//static.axept.io/sdk.js";
+        t.parentNode.insertBefore(e, t);
+    })(document, "script");
 </script>
 
 <script src="public/js/script.js"></script>
@@ -18,14 +20,51 @@ window.axeptioSettings = {
     <div class="footer-jaune">
         <h2 class="footer-jaune-titre">T’as tout pour toi !</h2>
         <p class="footer-jaune-soustitre">Un jour on parlera peut-être de toi dans cette newsletter, il ne faudrait pas manquer ça !
-            <br>Alors, abonne-toi !</p>
+            <br>Alors, abonne-toi !
+        </p>
 
-        <form class="footer-form">
+        <!-- <form class="footer-form">
             <div>
                 <input id="footer-newsletter" type="text" name="text" placeholder="reussite@gmail.com">
                 <input id="footer-submit" type="submit" value="Je m’inscris">
             </div>
-        </form>
+        </form> -->
+        <!-- Begin Mailchimp Signup Form -->
+        <!-- <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7_dtp.css" rel="stylesheet" type="text/css"> -->
+
+        <div id="mc_embed_signup">
+            <form action="https://tastoutpourtoi.us20.list-manage.com/subscribe/post?u=9120409457e5f9581d7ec805f&amp;id=7f9fd51774" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate footer-form" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                    <!-- <h2>T'as tout pour toi</h2> -->
+                    <!-- <div class="mc-field-group"> -->
+                    <!-- <label for="mce-EMAIL">Email Address </label> -->
+                    <input type="email" value="" name="EMAIL" class="required email" id="footer-newsletter">
+                    <!-- </div> -->
+
+                    <!-- </div> real people should not fill this in and expect good things - do not remove this or risk form bot signups -->
+                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9120409457e5f9581d7ec805f_7f9fd51774" tabindex="-1" value=""></div>
+                    <div class="optionalParent">
+                        <div class="clear foot">
+                            <input type="submit" value="Subscribe" name="subscribe" id="footer-submit" class="button">
+                            <!-- <p class="brandingLogo"><a href="http://eepurl.com/hO9JrL" title="Mailchimp - email marketing made easy and fun"><img src="https://eep.io/mc-cdn-images/template_images/branding_logo_text_dark_dtp.svg"></a></p> -->
+                        </div>
+                    </div>
+                    <!-- <div id="mce-responses" class="clear foot"> -->
+                </div>
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </form>
+        </div>
+        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+        <script type='text/javascript'>
+            (function($) {
+                window.fnames = new Array();
+                window.ftypes = new Array();
+                fnames[0] = 'EMAIL';
+            }(jQuery));
+            var $mcj = jQuery.noConflict(true);
+        </script>
+        <!--End mc_embed_signup-->
         <div class="footer-jaune-resaux">
             <div class="container">
                 <a href="https://www.linkedin.com/in/t-as-tout-pour-toi-365878226/" target="_blank"><span class="iconify" data-icon="akar-icons:linkedin-fill"></span></a>
